63b31462962805329d1c1639d9175af50d32fb07
dahoon <dahoon1006@gmail.com>
2022-05-11 12:32:07 +0900

    resume
    resume

---
+++ payload/introduce.ts
@@ -0,0 +1,16 @@
+import { IIntroduce } from '../component/introduce/IIntroduce';
+import { lastestUpdatedAt } from '../package.json';
+
+const introduce: IIntroduce.Payload = {
+  disable: false,
+
+  contents: [
+    '안녕하세요'
+  ],
+  sign: 'Love poem',
+  // sign: packageJson.author.name,
+  // latestUpdated: '2019-07-24',
+  latestUpdated: lastestUpdatedAt,
+};
+
+export default introduce;
